f7c2b563d4f2920e5289f7959e7571b564e23c01
Ben <benbright065@gmail.com>
2021-01-19 18:51:12 +0100

    home page

--- lib/screens/home/components/card-item-component.dart
+++ lib/screens/home/components/card-item-component.dart
@@ -9,71 +9,74 @@ class CardItemComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: GridView.count(
-          childAspectRatio: MediaQuery.of(context).size.height / 850,
-        shrinkWrap: true,
-          crossAxisCount: 2,
-          crossAxisSpacing: 5,
-          mainAxisSpacing: 5,
-        children: List.generate(images.length, (index){
-         return Card(
-           elevation: 3.0,
-            child: Container(
-              height: 300,
-              width: 200.0,
+      child: Container(
+        margin: EdgeInsets.only(left: 10, right: 10),
+        child: GridView.count(
+            childAspectRatio: MediaQuery.of(context).size.height / 850,
+          shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+          children: List.generate(images.length, (index){
+           return Card(
+             elevation: 3.0,
               child: Container(
-                padding: EdgeInsets.only(left: 10, right: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 120.0,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(images[index]),
-                              fit: BoxFit.contain
-                          )
+                height: 300,
+                width: 200.0,
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(names[index],
-                      style: defaultTextStyle,),
-                    Text(types[index],
-                      style: lightTextStyle,),
-                    Text(quantity[index],
-                      style: lightTextStyle,),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
+                      Container(
+                        height: 120.0,
                         decoration: BoxDecoration(
-                            color: greyColor,
-                          borderRadius: BorderRadius
-                              .all(Radius.circular(20))
+                            image: DecorationImage(
+                                image: AssetImage(images[index]),
+                                fit: BoxFit.contain
+                            )
                         ),
-                        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                        child: Text('\u20A6${prices[index]}',
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontFamily: 'ProximaNova"',
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                          ),),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(names[index],
+                        style: defaultTextStyle,),
+                      Text(types[index],
+                        style: lightTextStyle,),
+                      Text(quantity[index],
+                        style: lightTextStyle,),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: greyColor,
+                            borderRadius: BorderRadius
+                                .all(Radius.circular(20))
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                          child: Text('\u20A6${prices[index]}',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontFamily: 'ProximaNova"',
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        })),
+            );
+          })),
+      ),
     );
   }
 }
--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -3,14 +3,86 @@ import 'package:DROHealthPharmacy/screens/home/components/circle-button.dart';
 import 'package:DROHealthPharmacy/theme/style.dart';
 import 'package:flutter/material.dart';
 import 'package:mdi/mdi.dart';
+import 'package:hexcolor/hexcolor.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isSearchTextShown = false;
+  TextEditingController _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    Widget searchBar = TextField(
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'ProximaNova',
+        fontSize: 14,
+      ),
+      controller: _textController,
+      cursorColor: HexColor("#5D5D5D"),
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              borderSide: BorderSide(color: HexColor("#5D5D5D"))),
+          border: new OutlineInputBorder(
+            borderRadius: new BorderRadius.circular(20.0),
+          ),
+          prefixIcon: IconButton(
+            onPressed: (){
+
+            },
+            icon: Icon(Icons.search,
+                color: Colors.black54),
+          ),
+          suffixIcon: IconButton(
+            onPressed: (){
+              setState(() {
+                _textController.text= "";
+              });
+            },
+            icon: Icon(Icons.close,
+                 color: Colors.black54),
+          ),
+          hintText: 'Search',
+          hintStyle: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'ProximaNova',
+            fontSize: 14,
+          ),
+          contentPadding: const EdgeInsets.all(10.0)),
+      onChanged: (string) {
+        if(string!=""){
+          setState(() {
+            isSearchTextShown = true;
+          });
+        } else{
+          setState(() {
+            isSearchTextShown = false;
+          });
+        }
+        //check for text characters
+        // filteredData = users
+        //     .where((u) => ((u.first_name.toLowerCase() +
+        //     " " +
+        //     u.last_name.toLowerCase())
+        //     .contains(string.toLowerCase()) ||
+        //     u.industry.toLowerCase().contains(string.toLowerCase()) ||
+        //     u.country.toLowerCase().contains(string.toLowerCase())))
+        //     .toList();
+        // if (filteredData.isEmpty) {
+        //   setState(() {
+        //     isSearchingMore = true;
+        //   });
+        // } else {
+        //   setState(() {
+        //     isSearchingMore = false;
+        //   });
+        },
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -48,19 +120,71 @@ image: "assets/images/filter.png",
             ],
           ),
           SizedBox(
-            height: 30,
+            height: 15,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10, right: 10),
+              child: searchBar),
+          SizedBox(
+            height: 15,
           ),
           CardItemComponent(),
-          SizedBox(
-            height: 30,
-          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
+              width: double.infinity,
               decoration: BoxDecoration(
-                color: droPurple
+                borderRadius: BorderRadius.only(topLeft:
+                Radius.circular(30.0), topRight: Radius.circular(30.0)),
+                color: darkPurple
               ),
-              height: 100,
+              height: 80,
+              child: Column(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 20,
+                    child: new Icon(
+                      Icons.horizontal_rule_rounded,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      new Image.asset("assets/images/bag.png",
+                        color: Colors.white,
+                        height: 25,
+                        width: 60,),
+                      Text('Bag',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'ProximaNova"',
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),),
+                      Spacer(),
+                      Container(
+                        padding: EdgeInsets.all(14),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white
+                        ),
+                        child: Text('4',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'ProximaNova"',
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                          ),),
+                      ),
+                      SizedBox(
+                        width: 30,
+                      )
+                    ],
+                  ),
+                ],
+              ),
 
             ),
           )
