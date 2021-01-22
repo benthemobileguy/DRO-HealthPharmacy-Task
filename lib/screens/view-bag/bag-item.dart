7f7c851517edf7d9f61779f08f036154deff3743
Ben <benbright065@gmail.com>
2021-01-22 11:14:00 +0100

    show Dialog

--- lib/screens/view-bag/bag-item.dart
+++ lib/screens/view-bag/bag-item.dart
@@ -1,6 +1,7 @@
 import 'package:DROHealthPharmacy/bloc/default.dart';
 import 'package:DROHealthPharmacy/utils/global-variables.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 class BagItem extends StatelessWidget {
   const BagItem({
@@ -17,40 +18,32 @@ class BagItem extends StatelessWidget {
         itemBuilder: (context, index){
           return  Padding(
             padding: const EdgeInsets.only(bottom: 16),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 15,
-                ),
-                CircleAvatar(
-                    radius: 35,
-                    backgroundColor: Colors.white,
-                    child: Image.asset(
-                      mainBloc.productsInBag[index].image,
-                      fit: BoxFit.cover,
-                      width: 90.0,
-                      height: 90.0,
-                    )),
-                SizedBox(
-                  width: 15,
-                ),
-                Text(
-                  '1X',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: 'ProximaNova',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            child: GestureDetector(
+              onTap: (){
+                Fluttertoast.showToast(msg: "Not yet implemented",
+                    backgroundColor: Colors.redAccent,
+                    textColor: Colors.white);
+              },
+              child: Container(
+                child: Row(
                   children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    CircleAvatar(
+                        radius: 35,
+                        backgroundColor: Colors.white,
+                        child: Image.asset(
+                          mainBloc.productsInBag[index].image,
+                          fit: BoxFit.cover,
+                          width: 90.0,
+                          height: 90.0,
+                        )),
+                    SizedBox(
+                      width: 15,
+                    ),
                     Text(
-                      mainBloc.productsInBag[index].quantity,
+                      '1X',
                       style: TextStyle(
                         fontSize: 16,
                         fontFamily: 'ProximaNova',
@@ -59,33 +52,50 @@ class BagItem extends StatelessWidget {
                       ),
                     ),
                     SizedBox(
-                      height: 3,
+                      width: 15,
                     ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          mainBloc.productsInBag[index].quantity,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'ProximaNova',
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          mainBloc.productsInBag[index].type,
+                          style: TextStyle(
+                            fontSize: 12.5,
+                            fontFamily: 'ProximaNova',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
                     Text(
-                      mainBloc.productsInBag[index].type,
+                      '\u20A6 ${mainBloc.productsInBag[index].amount}',
                       style: TextStyle(
-                        fontSize: 12.5,
+                        fontSize: 16,
                         fontFamily: 'ProximaNova',
-                        fontWeight: FontWeight.w300,
+                        fontWeight: FontWeight.w700,
                         color: Colors.white,
                       ),
                     ),
+                    SizedBox(
+                      width: 15,
+                    )
                   ],
                 ),
-                Spacer(),
-                Text(
-                  '\u20A6 ${mainBloc.productsInBag[index].amount}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: 'ProximaNova',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  width: 15,
-                )
-              ],
+              ),
             ),
           );
         },
